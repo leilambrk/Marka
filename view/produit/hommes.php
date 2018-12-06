dd59a5348627f87bb0d7373923bfcab5363bc064
Senhaji Salma <salma.senhaji@etu.umontpellier.fr>
2018-12-06 17:57:07 +0100

    ok

--- view/produit/hommes.php
+++ view/produit/hommes.php
@@ -1,1 +1,24 @@
-<h1>hommes</h1>
+<h1>Tendances | Mode HOMMES</h1>
+
+<aside class="aside">
+<h3  id="pageHomme">Collection Hommes</h3>
+
+<ul class="list1">
+	<li><a href="">Jeans</a></li>
+	<li><a href="">Pulls</a></li>
+	<li><a href="">Vestes</a></li>
+	<li><a href="">Tenues de fêtes</a></li>
+	<li><a href="">T-shirt</a></li>
+	<li><a href="">Vêtements de sport</a></li>
+</ul>
+
+
+</aside>
+
+<article>
+
+<img src="images/jean1.jpg" />
+
+
+
+</article>
