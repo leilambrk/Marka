5307f82b4b3b8f34b2b05e7a67193597b1c43278
Senhaji Salma <salma.senhaji@etu.umontpellier.fr>
2018-12-06 17:07:37 +0100

    ok

--- view/accueil/accueil.php
+++ view/accueil/accueil.php
@@ -4,4 +4,18 @@
 
 <h2>Venez découvrir nos collections tendances et chics. </h2>
 
+<img src="images/homme.jpg" class="cat">
+<img src="images/femme.jpg" class="cat">
+<img src="images/enfants.jpg" class="cat">
 
+<form name="x" action="homme.php" method="post">
+<input type="submit" value="Collection Hommes" class="bouton">
+</form>
+
+<form name="x" action="femme.php" method="post">
+<input type="submit" value="Collection Femmes" class="bouton">
+</form>
+
+<form name="x" action="enfants.php" method="post">
+<input type="submit" value="Collection Enfants" class="bouton">
+</form>
