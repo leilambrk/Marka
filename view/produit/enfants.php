ff1d86a7d9adac48b92bd1a4dd3ff6899dc9f1c5
Senhaji Salma <salma.senhaji@etu.umontpellier.fr>
2018-12-07 13:34:10 +0100

    ok

--- view/produit/enfants.php
+++ view/produit/enfants.php
@@ -1,1 +1,51 @@
-<h1>enfants</h1>
+<h1>Tendances | Mode ENFANTS</h1>
+
+<aside class="aside">
+<h3 id="pageHomme">Collection Filles</h3>
+
+<ul class="list1">
+	<li><a href="">Jeans</a></li>
+	<li><a href="">T-shirt</a></li>
+	<li><a href="">Robes</a></li>
+	<li><a href="">Vestes</a></li>
+	<li><a href="">Tenues de fêtes</a></li>
+</ul>
+
+
+<h3  id="pageHomme">Collection Garçons</h3>
+
+<ul class="list1">
+	<li><a href="">Jeans</a></li>
+	<li><a href="">Chemises</a></li>
+	<li><a href="">Pull</a></li>
+	<li><a href="">Vestes</a></li>
+	<li><a href="">Tenues de fêtes</a></li>
+	
+
+
+
+
+</aside>
+
+
+
+
+
+<h2 class="h2"> Filles </h2>
+<img src="images/salopette.jpeg" alt="Jean" class="vet" />
+<img src="images/robefille.jpg" alt="Pull" class="vet" />
+<img src="images/tshirtfille.jpeg" alt="T-shirt" class="vet" />
+
+
+
+
+
+
+<h2 class="h2"> Garçons </h2>
+<img src="images/jeanf.jpg" alt="Jean" class="vet" />
+<img src="images/chemise.jpg" alt="Pull" class="vet" />
+<img src="images/to.jpg" alt="T-shirt" class="vet" />
+
+
+
+
--- view/produit/femmes.php
+++ view/produit/femmes.php
@@ -1,1 +1,25 @@
-<h1>femmes</h1>
+<h1>Tendances | Mode FEMMES</h1>
+
+<aside class="aside">
+<h3  id="pageHomme">Collection Femmes</h3>
+
+<ul class="list1">
+	<li><a href="">Jeans</a></li>
+	<li><a href="">Top</a></li>
+	<li><a href="">Robes</a></li>
+	<li><a href="">Vestes</a></li>
+	<li><a href="">Tenues de fêtes</a></li>
+	<li><a href="">Vêtements de sport</a></li>
+</ul>
+
+
+</aside>
+
+
+<img src="images/jeanf.jpg" alt="Jean" class="vet" />
+<img src="images/robe.jpg" alt="Pull" class="vet" />
+<img src="images/to.jpg" alt="T-shirt" class="vet" />
+
+
+
+
