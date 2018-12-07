8e2fcf6422fd347fdae0756990007d2dc5c28132
hamzayou3a <hamzayou3a@gmail.com>
2018-12-07 14:39:48 +0100

    push

--- config/Conf.php
+++ config/Conf.php
@@ -5,16 +5,16 @@ class Conf {
     static private $databases = array(
         // Le nom d'hote est webinfo a l'IUT
         // ou localhost sur votre machine
-        'hostname' => 'webinfo',
+        'hostname' => 'localhost',
         // A l'IUT, vous avez une BDD nommee comme votre login
         // Sur votre machine, vous devrez creer une BDD
-        'database' => 'senhajis',
+        'database' => 'marka',
         // A l'IUT, c'est votre login
         // Sur votre machine, vous avez surement un compte 'root'
-        'login' => 'senhajis',
+        'login' => 'root',
         // A l'IUT, c'est votre mdp (INE par defaut)
         // Sur votre machine personelle, vous avez creez ce mdp a l'installation
-        'password' => 'salma'
+        'password' => ''
     );
     // la variable debug est un boolean
     static private $debug = True;
--- view/profil/error.php
+++
@@ -1,1 +0,0 @@
-<h3>Erreur 404 - Page introuvable</h3>
