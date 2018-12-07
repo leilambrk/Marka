22f00faec155610b6af99cfdc6c91b8b1e20dfce
Leila MABROUK <mabroukl@icloud.com>
2018-12-07 15:04:36 +0100

    com

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 session_start();
 require_once 'lib/File.php';
 //session_start();
-//require_once File::build_path(array('lib','Security.php'));
-//require_once File::build_path(array('lib','Session.php'));
+require_once File::build_path(array('lib','Security.php'));
+require_once File::build_path(array('lib','Session.php'));
 require_once File::build_path(array("controller" , "routeur.php"));
 ?>
