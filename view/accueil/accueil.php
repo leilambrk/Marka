3ac966803dbe362cf0f2752808c535c38adfbc1a
hamzayou3a <hamzayou3a@gmail.com>
2018-12-13 16:59:54 +0100

    bip

--- view/accueil/accueil.php
+++ view/accueil/accueil.php
@@ -1,12 +1,24 @@
+
 <h1>La mode qui convient à tous.</h1>
 
-<img src="images/ban.jpg" alt="La boutique en ligne" class="ban"/>
+<img src="images/banniere.jpg" alt="La boutique en ligne" class="ban"/>
 
-<h2>Venez découvrir nos collections tendances et chics. </h2>
+
+<h2></h2>
+<h2 class="slog"> Marka, laissez vous guider par la mode ! </h2>
 
 <a href="?action=display1st&controller=produit"><img src="images/homme.jpg" alt="homme" class="cat"></a>
 <a href="?action=display2nd&controller=produit"><img src="images/femme.jpg" alt="femme"class="cat"></a>
 <a href="?action=display3rd&controller=produit"><img src="images/enfants.jpg" alt="enfants" class="cat"></a>
 
+<h2> Des nouveautés toutes les semaines pour un look branché ! </h2>
 
-<h2> Des nouveautés toues les semaines pour un look branché ! </h2>
+<div align="center">
+  <div class="contener_slideshow">
+    <div class="contener_slide">
+      <div class="slid_1"><img src="images/slid1.jpg" alt="robe"></div>
+      <div class="slid_2"><img src="images/slid2.jpg" alt="top"></div>
+      <div class="slid_3"><img src="images/slid3.jpg" alt="pull"></div>
+    </div>
+  </div>
+</div>
