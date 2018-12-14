142832829935550cbb08257aab11e6a7a910276e
Leila MABROUK <mabroukl@icloud.com>
2018-12-14 13:18:52 +0100

    font fini

--- view/utilisateur/connect.php
+++ view/utilisateur/connect.php
@@ -1,15 +1,15 @@
-<h2> Bienvenue sur le site Marka, veuillez vous connecter. </h2>
+<h2 class="titlet"> Bienvenue sur le site Marka, veuillez vous connecter. </h2>
 
 <form class="form" method="post" action="?action=connected&controller=utilisateur">
 
 
   <fieldset class="connect" >
     <p>
-      <label for="login_id">Adresse Mail</label> :
+      <label for="login_id" class="bal">Adresse Mail</label> :
       <input class="label" type="text" placeholder="Ex : mabroukl@icloud.com" name="email" id="email" required/>
     </p>
     <p>
-      <label for="pw_id">Mot de passe</label> :
+      <label for="pw_id" class="bal">Mot de passe</label> :
       <input class="label" type="password" placeholder="Ex : motdepasse " name="password" id="password" required/>
     </p>
     <p>
@@ -19,5 +19,5 @@
   </fieldset>
     
 </form>
-<h4 class="inscrire" > <a href="?action=create&controller=utilisateur" > Si vous n'êtes pas encore inscrit, inscrivez vous gratuitement sur notre site en cliquant ici. </a></h4>
+<h4 class="inscrire" > <a href="?action=create&controller=utilisateur" class="bal"> Si vous n'êtes pas encore inscrit, inscrivez vous gratuitement sur notre site en cliquant ici. </a></h4>
 
