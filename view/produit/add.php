6aa29858b8dd70080a2bc6ac017d6e8c89469fc0
Leila MABROUK <mabroukl@icloud.com>
2018-12-14 13:44:54 +0100

    formulaire

--- view/produit/add.php
+++ view/produit/add.php
@@ -1,27 +1,26 @@
-
+<h2 class="titlet"> Ajoutez un nouvel article. </h2>
   <form method="post" action="?action=adder&controller=produit">
     <!-- On recupere les infos avec la methode post et on redirige vers la sauvegarde dans la base de donnees -->
 
     <fieldset>
-        <legend>Formulaire d'inscription :</legend>
 
         <p>
-            <label for="nom">Nom de l'article :</label>
+            <label for="nom" class="bal">Nom de l'article :</label>
             <input type="text" placeholder="Levi's Jeans" name="nom" id="nom" required/>
 
         </p>
         <p>
-            <label for="taille">Taille :</label>
+            <label for="taille" class="bal">Taille :</label>
             <input type="text" placeholder="Ex : 42" name="taille" id="taille" required/>
         </p>
         <p>
-            <label for="prix">Prix  :</label>
+            <label for="prix" class="bal">Prix  :</label>
             <input type="prix" placeholder="Ex : 152" name="prix" id="prix" required/>
             <label for="€">€</label>
 
         </p>
         <p>
-          <label for="categorie"> Catégorie :</label>
+          <label for="categorie" class="bal"> Catégorie :</label>
           <select name='categorie'> <label for="categorie"> Catégorie :</label>
             <option value="Hommes">Hommes</option>
             <option value="Femmes">Femmes</option>
@@ -29,11 +28,11 @@
           </select>
         </p>
         <p>
-            <label for="photo">URL (photo du produit) :</label>
+            <label for="photo" class="bal">URL (photo du produit) :</label>
             <input type="photo" placeholder="Ex : http://image.noelshack.com/..... " name="photo" id="photo" required/>
         </p>
         <p>
-            <label for="description">Description du produit :</label>
+            <label for="description" class="bal">Description du produit :</label>
             <input type="text" placeholder="Ex : Jean de bonne qualité ..." name="description" id="description"/>
         </p>
         <p>
--- view/produit/updateProduit.php
+++ view/produit/updateProduit.php
@@ -1,6 +1,5 @@
+<h2 class="titlet"> Modifiez l'article. </h2>
 <fieldset>
-    <legend>Formulaire de modification d'article :</legend>
-
     <p>
       <?php
         //var_dump($idpa = $_GET['idProduit']);
@@ -8,7 +7,7 @@
         $idp = $idp->get('prix');
         //var_dump($idp);
         //var_dump($idpb= $idp->get('idProduit'));
-        echo '<label for="new_price">Modifiez le prix :</label><input type="price" id="newprice" name="newprice" value="' . $idp . '" required/><label for="€">€</label>';
+        echo '<label for="new_price" class="bal">Modifiez le prix :</label><input type="price" id="newprice" name="newprice" value="' . $idp . '" required/><label for="€">€</label>';
        ?>
     </p>
     <p>
@@ -16,7 +15,7 @@
         //var_dump($idpa = $_GET['idProduit']);
         $idp = ModelProduit::getProduitByIdProduit($_GET['idProduit']);
         $idp = $idp->get('description');
-        echo '<label for="new_desc">Modifiez la description :</label><input type="price" id="newdesc" name="newdesc" value="' . $idp . '" required/>';
+        echo '<label for="new_desc" class="bal">Modifiez la description :</label><input type="price" id="newdesc" name="newdesc" value="' . $idp . '" required/>';
        ?>
     </p>
     <p>
@@ -24,7 +23,7 @@
         //var_dump($idpa = $_GET['idProduit']);
         $idp = ModelProduit::getProduitByIdProduit($_GET['idProduit']);
         $idp = $idp->get('taille');
-        echo '<label for="new_size">Modifiez la taille :</label><input type="price" id="newsize" name="newsize" value="' . $idp . '" required/>';
+        echo '<label for="new_size" class="bal">Modifiez la taille :</label><input type="price" id="newsize" name="newsize" value="' . $idp . '" required/>';
        ?>
     </p>
 
