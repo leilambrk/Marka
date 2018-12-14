73d79545f52f9e5c10c6f24fa1b5432ac29d2caa
Senhaji Salma <salma.senhaji@etu.umontpellier.fr>
2018-12-14 12:58:47 +0100

    ok

--- view/panier/panier.php
+++ view/panier/panier.php
@@ -1,4 +1,4 @@
-<h1>Panier</h1>
+<h1>Votre panier</h1>
 
 
 <article>
@@ -7,18 +7,18 @@
 if ($tab != false){
 foreach ($tab as $prod => $values) {
   echo '<h3> '. $values[1] .' x </h3>';
-  echo '<div><img class="vet" src= "'. htmlspecialchars($values[0]->get('photo')) . '" alt="' . htmlspecialchars($values[0]->get('idProduit')). '"/><h2>'. htmlspecialchars($values[0]->get('nomProduit')) . '</h2><h5>' . htmlspecialchars($values[0]->get('prix')) . '€ </h5> Taille' . htmlspecialchars($values[0]->get('taille')) . '<p>' . htmlspecialchars($values[0]->get('description')) . '</p></div>';
+  echo '<div><img class="vet" src= "'. htmlspecialchars($values[0]->get('photo')) . '" alt="' . htmlspecialchars($values[0]->get('idProduit')). '"/> <div class="info"><h2>'. htmlspecialchars($values[0]->get('nomProduit')) . '</h2><h5>' . htmlspecialchars($values[0]->get('prix')) . '€ </h5> Taille' . htmlspecialchars($values[0]->get('taille')) . '<p>' . htmlspecialchars($values[0]->get('description')) . '</p></div></div>';
 }
 if ($tab != false){
 
-  echo '<h3><a href=?action=viderPanier&controller=panier> Vider le panier </a></h3>';
+  echo '<div class="info"><h2><a href=?action=viderPanier&controller=panier> Vider le panier </a></h2> </div>';
 }
 if ($tab == false){
 
   echo '<h3>Votre panier est vide</h3>';
 
 }
-echo '<h3><a href=?action=commander&controller=commande>Passer au paiement </a></h3>';
+echo '<div class="info"><a class="addpanier" href=?action=commander&controller=commande>Passer au paiement </a></div>';
 
 
 }
